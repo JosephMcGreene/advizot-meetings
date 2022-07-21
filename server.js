84e91881fa81ab25dfcb05a41cd7efd05ce072e4
Joseph McGreene <josephorianmcgreene@gmail.com>
2022-07-21 15:25:34 -0700

    Make another deployment attempt

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(
 
 app.use(express.json());
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //Routes
-app.get("/*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.post("/post", cors(), (req, res) => {
