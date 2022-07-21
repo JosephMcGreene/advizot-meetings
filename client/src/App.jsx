7cee969e40566a7f6f7bec0cb7fc693f894e8028
Joseph McGreene <josephorianmcgreene@gmail.com>
2022-07-21 13:30:37 -0700

    Get Node server working, commit before first Heroku deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+// ! Overall Goals:
+// !    - Easy and Fast to use
+// !    - Secure Server Sign-In
+// !    - Answers to sign-in questions to be used during the course of the meeting via a projector
+// !    - POST data to Coach Accountable to be stored as a metric for user later on
+// TODO (1) Add a modal for Kevin to edit the form each month
+// TODO (2) Clean up UX:
+// TODO 	 - Change the font of the user input on Responses component
+// TODO 	 - Format Responses component to display info properly, helpfully, and aesthetically
+// TODO 	 - Add some animations(?)
+// TODO (3) Add a Back-End or some other way to have data persist
+// TODO    - POST data to Coach Accountable. Add ability to remove the data as well.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//React
+import { useState } from "react";
+
+//Internal
+import logo from "./img/big-a.png";
+import MeetingForm from "./components/MeetingForm";
+import Responses from "./components/Responses";
+import "./scss/App.scss";
+
+export default function App() {
+	const [responses, setResponses] = useState([]);
+
+	async function submitResponses(userResponse) {
+		setResponses([...responses, userResponse]);
+
+		const serverResponse = await fetch("../../../post", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(userResponse),
+		});
+		const json = await serverResponse.json();
+	}
+
+	return (
+		<div className="App">
+			<header className="header">
+				<img src={logo} alt="Advizot logo" className="logo" />
+				<h1 className="h1">Welcome!</h1>
+			</header>
+			<MeetingForm onSubmit={(userResponse) => submitResponses(userResponse)} />
+			<Responses responses={responses} />
+		</div>
+	);
+
+	// return (
+	// 	<div className="App">
+	// 		<header className="App-header">
+	// 			<p>A simple React app.....</p>
+
+	// 			<a
+	// 				className="App-link"
+	// 				href="https://reactjs.org"
+	// 				target="_blank"
+	// 				rel="noopener noreferrer"
+	// 			>
+	// 				Learn React
+	// 			</a>
+	// 			<form action="../../post" method="post" className="form">
+	// 				<button type="submit">Connected?</button>
+	// 			</form>
+	// 		</header>
+	// 	</div>
+	// );
 }
-
-export default App;
--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
-const path = require("path");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const axios = require("axios");
-
-const publicPath = path.join(__dirname, "../meeting-sign-in", "public");
 
 //Middleware
 app.use(
@@ -15,16 +12,10 @@ app.use(
 );
 
 app.use(express.json());
-app.use(express.static(publicPath));
 
 //Routes
-app.get("*", (req, res) => {
-	res.sendFile(path.join(publicPath, "index.html"));
-});
-
 app.post("/post", cors(), (req, res) => {
 	console.log(req.body);
-	console.log(res.body);
 });
 
 //Server Start
