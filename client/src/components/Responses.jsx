7b71240292e2784682dc0cf52689bfec6c0ff0cb
Joseph McGreene <josephorianmcgreene@gmail.com>
2022-07-26 12:18:30 -0700

    Improve Responses layout

--- client/src/components/Responses.jsx
+++ client/src/components/Responses.jsx
@@ -8,59 +8,56 @@ export default function Responses({ responses }) {
 		<ul className="responses-ul">
 			{sortedResponses.map((response) => {
 				return (
-					<>
-						<li
-							className="response-li"
-							key={
-								response.name +
-								response.business +
-								response.personal +
-								response.relationships +
-								response.priority
-							}
-						>
-							<p className="response-name">
-								<strong>{response.name}</strong>
-							</p>
+					<li
+						className="response-li"
+						key={
+							response.name +
+							response.business +
+							response.personal +
+							response.relationships +
+							response.priority
+						}
+					>
+						<p className="response-name">
+							<strong>{response.name}</strong>
+						</p>
 
-							<p className="response-p">
-								<strong>Business</strong>
-								<br />
-								{response.business}
-							</p>
+						<p className="response-p">
+							<strong>Business</strong>
+							<br />
+							{response.business}
+						</p>
 
-							<p className="response-p">
-								<strong>Personal</strong>
-								<br />
-								{response.personal}
-							</p>
+						<p className="response-p">
+							<strong>Personal</strong>
+							<br />
+							{response.personal}
+						</p>
 
-							<p className="response-p">
-								<strong>People</strong>
-								<br />
-								{response.relationships}
-							</p>
+						<p className="response-p">
+							<strong>People</strong>
+							<br />
+							{response.relationships}
+						</p>
 
-							<p className="response-p">
-								<strong>Today's Issue</strong>
-								<br />
-								{response.monthlyIssue}
-							</p>
+						<p className="response-p">
+							<strong>Today's Issue</strong>
+							<br />
+							{response.monthlyIssue}
+						</p>
 
-							<p className="response-p">
-								<strong>Priority</strong>
-								<br />
-								{response.priority}
-							</p>
+						<p className="response-p">
+							<strong>Priority</strong>
+							<br />
+							{response.priority}
+						</p>
 
-							<p className="response-p">
-								<strong>Goal Before Next Meeting</strong>
-								<br />
-								{response.monthlyGoal}
-							</p>
-						</li>
-						<hr />
-					</>
+						<p className="response-p">
+							<strong>Goal Before Next Meeting</strong>
+							<br />
+							{response.monthlyGoal}
+						</p>
+					</li>
 				);
 			})}
 		</ul>
