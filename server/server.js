73e587ba4616fe5f1bd4764f83685f89cd4bade8
Joseph McGreene <josephorianmcgreene@gmail.com>
2022-08-07 21:35:26 -0700

    Update production serve routes

--- server/server.js
+++ server/server.js
@@ -20,9 +20,9 @@ app.use(mongoRoutes);
 
 //To serve in prod
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("./client/build"));
+	app.use(express.static("../client/build"));
 	app.get("*", cors(), (req, res) => {
-		res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+		res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
 	});
 }
 
