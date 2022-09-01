a2385a8e1a904d6ad0a1f9c630342167c7c0ef6d
Joseph McGreene <josephorianmcgreene@gmail.com>
2022-09-01 14:28:45 -0700

    Update Responses layout, refactor Responses list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@
 //!    - Secure Server Sign-In
 //!    - Answers to sign-in questions to be used during the course of the meeting via a projector
 //!    (- POST data to Coach Accountable to be stored as a metric for user later on)
-//TODO (1) Connect user to user response (display user's name next to their response)
+//TODO (1) Authorize users as Vistage Members to ensure only members can add responses.
 //TODO (2) Add user
 //TODO			- settings
 //TODO			- ability to delete/change their own responses
@@ -15,7 +15,7 @@ import { useState, useEffect } from "react";
 import "./scss/App.scss";
 import Header from "./components/Header";
 import MeetingForm from "./components/form/MeetingForm";
-import Responses from "./components/Responses";
+import Responses from "./components/responses/Responses";
 import Footer from "./components/Footer";
 //External
 import axios from "axios";
@@ -26,66 +26,66 @@ export default function App() {
 
 	//=====EFFECTS=====
 	useEffect(() => {
-		/**
-		 * makes ajax request for details on the current user, defined to keep useEffect happy by remaining "synchronous" *eyeroll*
-		 * @param {String} url
-		 */
-		async function getCurrentUser(url) {
-			try {
-				const current_user = await axios(url, {
-					headers: {
-						Accept: "application/json",
-						"Content-Type": "application/json",
-					},
-				});
-				setCurrentUser(current_user.data);
-			} catch (error) {
-				console.error(error);
-			}
-		}
-		getCurrentUser("/auth/current_user");
+		getCurrentUser();
 	}, []);
 
 	useEffect(() => {
-		/**
-		 * fetches existing user responses from MongoDB and updates state accordingly. See server/routes/db.js
-		 */
-		async function getExistingResponses() {
-			try {
-				const existingResponses = await axios({
-					method: "get",
-					url: "/db/responses",
-					headers: {
-						Accept: "application/json",
-						"Content-Type": "application/json",
-					},
-				});
-				if (existingResponses.data && existingResponses.data.length > 0) {
-					setResponses([...existingResponses.data]);
-				}
-			} catch (error) {
-				console.error(error);
-			}
-		}
 		getExistingResponses();
 	}, []);
 
 	//=====HELPERS=====
 	/**
-	 * Takes in and distributes responses from MeetingForm.js to the appropriate places: MongoDB and/or Coach Accountable. See server/routes/db.js
-	 * @param {Object} userResponse json body to be posted and displayed to the users
+	 * makes request for info on the current user
+	 */
+	async function getCurrentUser() {
+		try {
+			const current_user = await axios("/auth/current_user", {
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+			});
+			setCurrentUser(current_user.data);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	/**
+	 * fetches existing user responses from MongoDB and updates state accordingly. See server/routes/db.js
+	 */
+	async function getExistingResponses() {
+		try {
+			const existingResponses = await axios({
+				method: "get",
+				url: "/db/responses",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+			});
+			if (existingResponses.data && existingResponses.data.length > 0) {
+				setResponses([...existingResponses.data]);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	/**
+	 * Takes in response from MeetingForm.js adds it to the database. See /routes/db.js
+	 * @param {Object} userResponse json body to be added to the database and displayed to the users
 	 */
 	async function submitResponses(userResponse) {
 		userResponse.userName = `${currentUser.firstName} ${currentUser.lastName}`;
 		userResponse.date = Date.now();
 		await axios({ method: "post", url: "/db/responses", data: userResponse });
-		console.log(userResponse);
 		setResponses([...responses, userResponse]);
 	}
 
 	// DELETES EVERY SINGLE USER RESPONSE ENTRY IN DATABASE
 	/**
-	 * Deletes the current slate of documents from MongoDB. See server/routes/db.js
+	 * Deletes the current slate of documents from MongoDB. See /routes/db.js
 	 */
 	async function deleteAllResponses() {
 		if (responses.length === 0) {
@@ -101,9 +101,9 @@ export default function App() {
 	}
 
 	return (
-		<div className="App">
+		<div>
 			<Header currentUser={currentUser} />
-			{/* Personal Welcome Message: */}
+			{/* Personalized Welcome Message for logged-in users: */}
 			{currentUser ? (
 				<h1 className="welcome">Hello, {currentUser.firstName}!</h1>
 			) : (
@@ -115,11 +115,13 @@ export default function App() {
 				currentUser={currentUser}
 			/>
 			<Responses currentUser={currentUser} responses={responses} />
+
 			<button className="btn" onClick={() => deleteAllResponses()}>
 				Delete All Responses
 				<br />
 				(Please use after testing)
 			</button>
+
 			<Footer />
 		</div>
 	);
---
+++ client/src/components/responses/Responses.jsx
@@ -0,0 +1,27 @@
+import Response from "./Response";
+
+export default function Responses({ responses }) {
+	const sortedResponses = responses.sort((a, b) => {
+		if (a.priority < b.priority) return -1;
+		return 1;
+	});
+
+	return (
+		<ul className="responses-ul">
+			{sortedResponses.map((response) => {
+				return (
+					<Response
+						key={
+							response.date +
+							response.business +
+							response.personal +
+							response.relationships +
+							response.priority
+						}
+						responseBody={response}
+					/>
+				);
+			})}
+		</ul>
+	);
+}
