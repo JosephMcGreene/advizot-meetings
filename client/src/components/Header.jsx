79371b190a6926a0e72695079400f61ead3f1a02
Joseph McGreene <josephorianmcgreene@gmail.com>
2022-09-22 12:10:36 -0700

    reset HEAD to appropriate place

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,30 +2,51 @@ import { useState, useContext } from "react";
 import { UserContext } from "../App";
 import advizotLogo from "../img/Original on Transparent.png";
 import Login from "./modals/Login";
+import MeetingCode from "./modals/MeetingCode";
 
-export default function Header() {
+export default function Header({ onSubmit }) {
+	const [showMeetingCode, setShowMeetingCode] = useState(false);
 	const [showLogin, setShowLogin] = useState(false);
 	const currentUser = useContext(UserContext);
 
 	return (
 		<header className="header">
 			<nav className="nav-bar">
-				<img src={advizotLogo} alt="Advizot logo" className="logo" />
-				<div className="actions-container">
-					{/* Show sign out link if logged in, or sign in if logged out */}
-					{currentUser ? (
-						<a href="/auth/logout">
-							<button className="btn">Sign out</button>
-						</a>
-					) : (
-						<button className="btn" onClick={() => setShowLogin(!showLogin)}>
-							Sign in
-						</button>
-					)}
-				</div>
+				<ul className="nav-ul">
+					<li className="logo-wrapper">
+						<img src={advizotLogo} alt="Advizot logo" className="logo" />
+					</li>
+					<li className="actions-container">
+						{/* Show sign out link if logged in, or sign in if logged out */}
+						{currentUser ? (
+							<>
+								<button
+									className="btn"
+									onClick={() => setShowMeetingCode(!showMeetingCode)}
+								>
+									Enter Meeting
+								</button>
+								<a href="/auth/logout">
+									<button className="btn">Sign out</button>
+								</a>
+							</>
+						) : (
+							<button className="btn" onClick={() => setShowLogin(!showLogin)}>
+								Sign in
+							</button>
+						)}
+					</li>
+				</ul>
 			</nav>
 
-			<Login showLogin={showLogin} onClose={() => setShowLogin(!showLogin)} />
+			{/* don't always need to see the modals */}
+			{showLogin && <Login onClose={() => setShowLogin(!showLogin)} />}
+			{showMeetingCode && (
+				<MeetingCode
+					onClose={() => setShowMeetingCode(!showMeetingCode)}
+					onSubmit={onSubmit}
+				/>
+			)}
 		</header>
 	);
 }
