3042f61dc7c3c5ca3adb65585f4c8e45fb9bed87
Joseph McGreene <josephorianmcgreene@gmail.com>
2023-03-14 13:49:14 +0100

    Add functionality to useUser to refetch data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,63 +1,30 @@
 import { createContext } from "react";
-//External
-import { Routes, Route } from "react-router-dom";
 //Internal
 import useUser from "./components/hooks/useUser";
 import "./assets/scss/App.scss";
 //Components
 import Header from "./components/Header";
+import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
-import UsersOnly from "./components/utilities/UsersOnly";
-import PresentUsersOnly from "./components/utilities/PresentUsersOnly";
-import Meeting from "./components/pages/Meeting";
 
 //Context for logged in user data:
 export const UserContext = createContext();
 
 export default function App() {
-  const user = useUser();
-
-  /**
-   * Assesses whether or not the passcode the user entered is valid and correct
-   * @param {String} inputCode the code the user entered
-   */
-  // async function checkPasscode(inputCode) {
-  //   const response = await axiosFetch("post", "/auth/code", {
-  //     enteredCode: inputCode,
-  //   });
-  //   setCurrentUser(response.data);
-  // }
+  const [user, fetchUser] = useUser(); //fetches user data, enables refetching
 
   return (
     <div className="App">
       <UserContext.Provider value={user}>
         <Header />
 
-        <main className="main-content">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <h1 className="welcome">
-                  Welcome! <br /> Please sign in to continue.
-                </h1>
-              }
-            />
-
-            <Route
-              path="/meeting"
-              element={
-                <UsersOnly>
-                  <PresentUsersOnly
-                  // onSubmitPasscode={(inputCode) => checkPasscode(inputCode)}
-                  >
-                    <Meeting />
-                  </PresentUsersOnly>
-                </UsersOnly>
-              }
-            />
-          </Routes>
-        </main>
+        <MainContent
+          onSubmitPasscode={(inputCode) => {
+            fetchUser("post", "/auth/code", {
+              enteredCode: inputCode,
+            });
+          }}
+        />
 
         <Footer />
       </UserContext.Provider>
--- client/src/components/hooks/useUser.js
+++ client/src/components/hooks/useUser.js
@@ -17,5 +17,5 @@ export default function useUser() {
     }
   }
 
-  return user;
+  return [user, fetchUser];
 }
