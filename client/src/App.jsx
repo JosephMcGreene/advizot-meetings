af80fb29389b9e7b9abe4d8257f608ca34d36d9b
Joseph McGreene <josephorianmcgreene@gmail.com>
2023-03-14 11:58:31 +0100

    Add useUser hook for logging in users

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,63 @@
-import { useState, useEffect, createContext } from "react";
+import { createContext } from "react";
+//External
+import { Routes, Route } from "react-router-dom";
 //Internal
-import { axiosFetch } from "./helpers";
+import useUser from "./components/hooks/useUser";
 import "./assets/scss/App.scss";
 //Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import MainContent from "./components/MainContent";
+import UsersOnly from "./components/utilities/UsersOnly";
+import PresentUsersOnly from "./components/utilities/PresentUsersOnly";
+import Meeting from "./components/pages/Meeting";
 
 //Context for logged in user data:
 export const UserContext = createContext();
 
 export default function App() {
-  const [currentUser, setCurrentUser] = useState({});
-
-  useEffect(() => {
-    getCurrentUser();
-  }, []);
-
-  //=====HELPERS=====
-  /**
-   * makes request for info on the current user and updates currentUser state accordingly
-   */
-  async function getCurrentUser() {
-    const response = await axiosFetch("get", "/auth/current_user");
-    setCurrentUser(response.data);
-  }
+  const user = useUser();
 
   /**
    * Assesses whether or not the passcode the user entered is valid and correct
    * @param {String} inputCode the code the user entered
    */
-  async function checkPasscode(inputCode) {
-    const response = await axiosFetch("post", "/auth/code", {
-      enteredCode: inputCode,
-    });
-    setCurrentUser(response.data);
-  }
+  // async function checkPasscode(inputCode) {
+  //   const response = await axiosFetch("post", "/auth/code", {
+  //     enteredCode: inputCode,
+  //   });
+  //   setCurrentUser(response.data);
+  // }
 
   return (
     <div className="App">
-      <UserContext.Provider value={currentUser}>
+      <UserContext.Provider value={user}>
         <Header />
 
-        <MainContent
-          onSubmitPasscode={(inputCode) => checkPasscode(inputCode)}
-        />
+        <main className="main-content">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <h1 className="welcome">
+                  Welcome! <br /> Please sign in to continue.
+                </h1>
+              }
+            />
+
+            <Route
+              path="/meeting"
+              element={
+                <UsersOnly>
+                  <PresentUsersOnly
+                  // onSubmitPasscode={(inputCode) => checkPasscode(inputCode)}
+                  >
+                    <Meeting />
+                  </PresentUsersOnly>
+                </UsersOnly>
+              }
+            />
+          </Routes>
+        </main>
 
         <Footer />
       </UserContext.Provider>
---
+++ client/src/components/hooks/useUser.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { axiosFetch } from "../../helpers";
+
+export default function useUser() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetchUser("get", "/auth/current_user");
+  }, []);
+
+  async function fetchUser(method, url, data = null) {
+    try {
+      const response = await axiosFetch(method, url, data);
+      setUser(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return user;
+}
