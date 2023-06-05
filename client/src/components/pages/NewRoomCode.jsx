af78a9e60e31632c3a5ff7a545943ad243b5321c
Joseph McGreene <josephorianmcgreene@gmail.com>
2023-06-05 15:17:58 +0200

    Version 0.3.1

--- client/src/components/pages/NewRoomCode.jsx
+++ client/src/components/pages/NewRoomCode.jsx
@@ -11,6 +11,10 @@ export default function NewRoomCode() {
   const [loading, setLoading] = useState(false);
   const user = useContext(UserContext);
 
+  /**
+   * Calls the server to update the room code and send it back to the user, or only send the existing room code, then updatews the user's local storage with the new room code
+   * @param {boolean} needNewCode whether or not the user has decided to create a new room code
+   */
   async function setRoomCode(needNewCode) {
     try {
       setLoading(true);
