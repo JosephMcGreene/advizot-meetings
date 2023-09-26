b382cb9e72c085a5a47b404c46568516f81304b4
Joseph McGreene <josephorianmcgreene@gmail.com>
2023-09-25 18:36:53 -0700

    Add styles to toasts

--- client/src/shared/ToastList.jsx
+++ client/src/shared/ToastList.jsx
@@ -5,10 +5,28 @@ import Toast from "./Toast";
 export default function ToastList({ data, position, removeToast }) {
   const listRef = useRef(null);
 
+  const sortedData = position.includes("bottom")
+    ? [...data].reverse()
+    : [...data];
+
+  useEffect(() => {
+    handleScrolling(listRef.current);
+  }, [position, data]);
+
+  function handleScrolling(element) {
+    const isTopPosition = ["top-left", "top-right"].includes(position);
+
+    if (isTopPosition) {
+      element?.scrollTo(0, element.scrollHeight);
+    } else {
+      element?.scrollTo(0, 0);
+    }
+  }
+
   return (
-    data.length > 0 && (
+    sortedData.length > 0 && (
       <ul className={`toast-list ${position}`} aria-live="assertive">
-        {data.map((toast) => (
+        {sortedData.map((toast) => (
           <Toast
             altText={toast.altText}
             key={toast.id}
