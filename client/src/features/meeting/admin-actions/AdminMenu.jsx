040568a6689ffc89afe8b74fb5648057ede5c32f
Joseph McGreene <josephorianmcgreene@gmail.com>
2023-12-17 19:46:16 -0800

    Add user nav to profile picture

--- client/src/features/meeting/admin-actions/AdminMenu.jsx
+++ client/src/features/meeting/admin-actions/AdminMenu.jsx
@@ -1,4 +1,4 @@
-import verticalEllipse from "../../../assets/img/ellipsis-vertical-solid.svg";
+import slidersIcon from "../../../assets/img/ellipsis-vertical-solid.svg";
 
 export default function AdminMenu({ children, actionToggle, className }) {
   return (
@@ -10,7 +10,7 @@ export default function AdminMenu({ children, actionToggle, className }) {
         className="user-action-btn"
         onClick={() => actionToggle()}
       >
-        <img src={verticalEllipse} alt="user actions" className="dots-icon" />
+        <img src={slidersIcon} alt="user actions" className="dots-icon" />
       </button>
     </nav>
   );
