65f31a5e7c6348719b79b981a8a2221ca3a09be3
Joseph McGreene <josephorianmcgreene@gmail.com>
2024-03-18 13:28:39 -0700

    Add controlled selects to user edit form

--- client/src/features/meeting/admin-actions/MemberEditModal.jsx
+++ client/src/features/meeting/admin-actions/MemberEditModal.jsx
@@ -5,10 +5,18 @@ import useMemberEdits from "../../../hooks/useMemberEdits";
 import LoadingSpinner from "../../../shared/LoadingSpinner";
 
 export default function MemberEditModal({ handleClose, currentGroup }) {
-  const [selectedMemberID, setSelectedMemberID] = useState("");
-  const [userNotSelected, setUserNotSelected] = useState(true);
+  const [selectedUser, setSelectedUser] = useState("");
+  const [memberEditsDisabled, setMemberEditsDisabled] = useState(true);
   const [usersToEdit, loading, handleEditSubmit, deleteMember] =
     useMemberEdits(currentGroup);
+
+  function selectMember(eventTargetVal) {
+    setSelectedUser(
+      usersToEdit.find((user) => user.advizotID === eventTargetVal)
+    );
+
+    setMemberEditsDisabled(false);
+  }
 
   return (
     <div className="modal-body">
@@ -16,10 +24,14 @@ export default function MemberEditModal({ handleClose, currentGroup }) {
         {loading ? (
           <LoadingSpinner />
         ) : (
-          <label htmlFor="advizotID">
+          <label htmlFor="memberSelect">
             Select a Member
-            <select name="advizotID" id="advizotID" className="rating-select">
-              <option value="">-- Select a Member --</option>
+            <select
+              onChange={(e) => selectMember(e.target.value)}
+              id="memberSelect"
+              className="rating-select"
+            >
+              <option value="select">-- Select a Member --</option>
               {usersToEdit.map((user, index) => (
                 <option value={user.advizotID} key={user.advizotID + index}>
                   {user.firstName} {user.lastName}
@@ -29,35 +41,38 @@ export default function MemberEditModal({ handleClose, currentGroup }) {
           </label>
         )}
 
-        <label htmlFor="howToEdit">
-          How to Edit
-          <select name="howToEdit" id="howToEdit" className="rating-select">
-            <option value="">-- Select One --</option>
-            <option value="move">Move to New Group</option>
-            <option value="edit">Edit Profile</option>
-          </select>
-        </label>
+        {!memberEditsDisabled && (
+          <label htmlFor="howToEdit">
+            How to Edit
+            <select name="howToEdit" id="howToEdit" className="rating-select">
+              <option value="">-- Select One --</option>
+              <option value="move">Move to New Group</option>
+              <option value="edit">Edit Profile</option>
+            </select>
+          </label>
+        )}
 
-        <label htmlFor="groupToPlace">
-          Place Into Group
-          <select
-            name="groupToPlace"
-            id="groupToPlace"
-            className="rating-select"
-          >
-            <option value="">-- Select a Group --</option>
-            <option value="CE5660">CE5660</option>
-            <option value="KEY9330">KEY9330</option>
-            <option value="CE4659">CE4659</option>
-          </select>
-        </label>
+        {!memberEditsDisabled && (
+          <label htmlFor="groupToPlace">
+            Place Into Group
+            <select
+              name="groupToPlace"
+              id="groupToPlace"
+              className="rating-select"
+            >
+              <option value="">-- Select a Group --</option>
+              <option value="CE5660">CE5660</option>
+              <option value="KEY9330">KEY9330</option>
+              <option value="CE4659">CE4659</option>
+            </select>
+          </label>
+        )}
 
         {/* //TODO Add functionality to allow admins to delete members using this button */}
-        {usersToEdit.length > 0 && (
+        {!memberEditsDisabled && (
           <button
             type="button"
             className="delete-member-btn"
-            disabled={userNotSelected}
             //TODO Get access to member's advizotID from the Select component above
             onClick={() => deleteMember()}
           >
