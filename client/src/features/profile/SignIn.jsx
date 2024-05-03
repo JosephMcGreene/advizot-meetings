d4cd96a9a479e9c4442050c71b69301b386ca375
JosephMcGreene <josephorianmcgreene@gmail.com>
2024-05-03 09:58:30 +0200

    Add styles to queried profile sign-ins

--- client/src/features/profile/SignIn.jsx
+++ client/src/features/profile/SignIn.jsx
@@ -1,11 +1,16 @@
+import { ReactComponent as EditPen } from "../../assets/img/pen-solid.svg";
+
 export default function SignIn({ signIn }) {
   // TODO Figure Out exactly what needs to be displayed in this component
   return (
     <li className="profile-sign-in">
+      <EditPen className="edit-pen" />
       {/* TODO Format Date */}
       <h3>{signIn.date}</h3>
 
-      <p>{signIn.monthlyGoal}</p>
+      <p>
+        <u>Goal:</u> {signIn.monthlyGoal}
+      </p>
     </li>
   );
 }
