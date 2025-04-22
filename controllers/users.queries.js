9b39f09d6bc4cfa9edc496091a988b3577ad7894
JosephMcGreene <josephorianmcgreene@gmail.com>
2025-04-22 16:17:48 -0700

    Finish users.queries.js

--- controllers/users.queries.js
+++ controllers/users.queries.js
@@ -1,4 +1,10 @@
 import User from "../models/User.js";
+import { v4 as uuidv4 } from "uuid";
+import { userRoles, groups } from "./userRoles.js";
+
+export function addProviderID(email, provider, id) {
+  return User.findOneAndUpdate({ email: email }, { [provider]: id });
+}
 
 export function deleteUserDB(id) {
   User.deleteOne(id);
@@ -6,6 +12,10 @@ export function deleteUserDB(id) {
 
 export function getOneUser(profileEmail) {
   return User.findOne({ email: profileEmail });
+}
+
+export function getOneUserByID(id) {
+  return User.findById(id);
 }
 
 export function getUsersInGroup(group) {
@@ -16,6 +26,19 @@ export function moveUser(id, groupToPlace) {
   User.findByIdAndUpdate(id, { group: groupToPlace });
 }
 
-export function addProviderID(email, provider, id) {
-  return User.findOneAndUpdate({ email: email }, { [provider]: id });
+export async function saveNewUser(profile, provider) {
+  const newUser = await new User({
+    firstName: profile.name.givenName,
+    lastName: profile.name.familyName,
+    [provider]: profile.id,
+    email: profile.emails[0].value,
+    photo: profile.photos[0].value,
+    advizotID: uuidv4(),
+    role: userRoles.MEMBER,
+    group: groups.GUEST,
+    hasMeetingCode: false,
+  });
+  await newUser.save();
+
+  return newUser;
 }
